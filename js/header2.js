339ae89f56b5c1f625110dc889f71844c2e9fa5f
ylia-kano <44111823+ylia-kano@users.noreply.github.com>
2019-04-10 16:49:58 +0400

    Update header2.js

--- js/header2.js
+++ js/header2.js
@@ -1,3 +1,3 @@
 $(function (){ 
-    $('body').prepend('<header><div class="site-logo"><a href="#"><img src="image/logo.png"></a></div><div class="nav-menu"><ul><li class="nav-menu__item nav-menu__home"><a href="#">Home</a></li><li class="nav-menu__item"><a href="#">About<a></li><li class="nav-menu__item"><a href="#">Contact</a></li></ul></div></header>');
+    $('body').prepend('<header><div class="site-logo"><a href="#"><img src="image/logo.png"></a></div><div class="nav-menu"><ul><li class="nav-menu__item nav-menu__home"><a href="/home">Home</a></li><li class="nav-menu__item"><a href="/about">About<a></li><li class="nav-menu__item"><a href="#">Contact</a></li></ul></div></header>');
 });
