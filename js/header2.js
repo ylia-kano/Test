b28e3e07f2dfef61180b1fc115051cd3488b0aec
ylia-kano <44111823+ylia-kano@users.noreply.github.com>
2019-04-10 16:48:00 +0400

    Add files via upload

---
+++ js/header2.js
@@ -0,0 +1,3 @@
+$(function (){ 
+    $('body').prepend('<header><div class="site-logo"><a href="#"><img src="image/logo.png"></a></div><div class="nav-menu"><ul><li class="nav-menu__item nav-menu__home"><a href="#">Home</a></li><li class="nav-menu__item"><a href="#">About<a></li><li class="nav-menu__item"><a href="#">Contact</a></li></ul></div></header>');
+});
